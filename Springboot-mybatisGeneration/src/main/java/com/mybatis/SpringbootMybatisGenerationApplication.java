527940454d0675af2ec4a8b8ec2ce2034e1473e3
yuliangbing <913932453@qq.com>
2018-12-04 09:47:15 +0800

    初始化

---
+++ Springboot-mybatisGeneration/src/main/java/com/mybatis/SpringbootMybatisGenerationApplication.java
@@ -0,0 +1,12 @@
+package com.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisGenerationApplication.class, args);
+	}
+}
